844d5a0a62d9d3e1e17cf0db275fcadbdc7c2ff3
kubbar <kubbarr@hotmail.com>
2023-01-02 09:09:53 +0300

    Update NET25TV.py

--- scripts/NET25TV.py
+++ scripts/NET25TV.py
@@ -38,6 +38,8 @@ def grab(url):
     print(f"{link[start : end]}")
 
 print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
 with open('../NET25TV_info.txt') as f:
     for line in f:
