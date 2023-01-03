86885b96557b00332eefbdf69c1a27ffdb8b2322
kubbar <kubbarr@hotmail.com>
2023-01-03 07:29:11 +0300

    Update and rename D-PTVPhilippines.sh to D.PTVPhilippines.sh

--- D.PTVPhilippines.sh
+++ D.PTVPhilippines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
-python3 D-PTVPhilippines.py > ../D-PTVPhilippines.m3u8
+python3 D.PTVPhilippines.py > ../D.PTVPhilippines.m3u8
 
 echo m3u grabbed
