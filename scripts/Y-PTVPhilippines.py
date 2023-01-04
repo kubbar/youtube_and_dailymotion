a104a8403e34979ff4545c81cf6c5fdcdfc594ef
kubbar <kubbarr@hotmail.com>
2023-01-04 08:05:44 +0300

    Update Y-PTVPhilippines.py

--- scripts/Y-PTVPhilippines.py
+++ scripts/Y-PTVPhilippines.py
@@ -1,58 +1,56 @@
-#! /usr/bin/python3
+async function dashUrl(ytUrl) {
+  return await fetch(ytUrl)
+    .then(async (r) => await r.text())
+    .then((r) => r.match(/(?<=dashManifestUrl":").+?(?=",)/g)[0]);
+}
 
-banner = r'''
-'''
+async function hlsUrl(ytUrl) {
+  return await fetch(ytUrl)
+    .then(async (r) => await r.text())
+    .then((r) => r.match(/(?<=hlsManifestUrl":").*\.m3u8/g)[0]);
+}
 
-import requests
-import os
-import sys
+async function handleRequest(request) {
+  let pathName = new URL(request.url).pathname.split("/");
+  switch (pathName[3]) {
+    case "master.mpd":
+      try {
+        return Response.redirect(
+          await dashUrl(
+            "https://www.youtube.com/@marinai" + pathName[2] + "/live"
+          ),
+          302
+        );
+      } catch (err) {
+        return Response.redirect(
+          await dashUrl("https://www.youtube.com/watch?v=NjRaFa1ZGLI" + pathName[2]),
+          302
+        );
+      }
+    case "master.m3u8":
+      try {
+        return Response.redirect(
+          await hlsUrl(
+            "https://www.youtube.com/@marinai" + pathName[2] + "/live"
+          ),
+          302
+        );
+      } catch (err) {
+        return Response.redirect(
+          await hlsUrl("https://www.youtube.com/watch?v=NjRaFa1ZGLI" + pathName[2]),
+          302
+        );
+      }
+    default:
+      console.log("Error: Invalid path. Use master.mpd or master.m3u8");
+      break;
+  }
+}
 
-windows = False
-if 'win' in sys.platform:
-    windows = True
-
-def grab(url):
-    response = s.get(url, timeout=15).text
-    if '.m3u8' not in response:
-        response = requests.get(url).text
-        if '.m3u8' not in response:
-            if windows:
-                print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
-                return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
-                return
-    end = response.find('.m3u8') + 5
-    tuner = 100
-    while True:
-        if 'https://' in response[end-tuner : end]:
-            link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
-            break
-        else:
-            tuner += 5
-    print(f"{link[start : end]}")
-
-s = requests.Session()
-with open('../Y-PTVPhilippines_info.txt') as f:
-    for line in f:
-        line = line.strip()
-        if not line or line.startswith('~~'):
-            continue
-        if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
-        else:
-            grab(line)
-
-            
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+addEventListener("fetch", async (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
