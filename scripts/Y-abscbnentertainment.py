2fb31ebccbb0df395245454cecc55cb129f41546
kubbar <kubbarr@hotmail.com>
2023-01-05 08:24:08 +0300

    Update Y-abscbnentertainment.py

--- scripts/Y-abscbnentertainment.py
+++ scripts/Y-abscbnentertainment.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 
+banner = r'''
+'''
 
 import requests
 import os
@@ -12,14 +14,17 @@ if 'win' in sys.platform:
 def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
-        if windows:
-            print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
-            return
-        os.system(f'wget {url} -O temp.txt')
-        response = ''.join(open('temp.txt').readlines())
+        response = requests.get(url).text
         if '.m3u8' not in response:
-            print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
-            return
+            if windows:
+                print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
+                return
+            #os.system(f'wget {url} -O temp.txt')
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://raw.githubusercontent.com/kubbar/youtube_and_dailymotion/main/notwork/notwork.m3u8')
+                return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
@@ -30,15 +35,9 @@ def grab(url):
             break
         else:
             tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
-    hd = streams[-1].strip()
-    st = hd.find('http')
-    print(hd[st:].strip())
-    #print(f"{link[start : end]}")
+    print(f"{link[start : end]}")
 
-print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000,FRAME-RATE=60.000')
+print(banner)
 s = requests.Session()
 with open('../Y-abscbnentertainment_info.txt') as f:
     for line in f:
@@ -51,6 +50,7 @@ with open('../Y-abscbnentertainment_info.txt') as f:
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
             tvg_id = line[3].strip()
+            
         else:
             grab(line)
             
