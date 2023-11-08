de0098197b2c3ec084da8c312c0cc33286054ded
kubbar <kubbarr@hotmail.com>
2023-11-08 04:01:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
-from flask import Flask, redirect, request, Response
 import requests
+import sys
 
-app = Flask(__name__)
+# قراءة الرابط من ملف نصي
+with open('app_info.txt', 'r') as file:
+    config_url = file.read().strip()  # يحذف المسافات البيضاء والأسطر الجديدة
 
-@app.route('/get_m3u8')
-def get_m3u8():
-    config_url = 'https://player.vimeo.com/video/881909784/config?h=0c1725d19a'
-    try:
-        # جلب بيانات التكوين من Vimeo
-        config_response = requests.get(config_url)
-        config_response.raise_for_status()  # سيقوم بإثارة خطأ إذا كان الطلب ليس 200
-        config_data = config_response.json()
+try:
+    # جلب بيانات التكوين من Vimeo
+    response = requests.get(config_url)
+    response.raise_for_status()  # سيقوم بإثارة خطأ إذا كان الطلب ليس 200
+    config_data = response.json()
 
-        # استخراج رابط m3u8 من بيانات التكوين
-        m3u8_url = config_data['request']['files']['hls']['cdns']['fastly_live']['url']
+    # استخراج رابط m3u8
+    m3u8_url = config_data['request']['files']['hls']['cdns']['fastly_live']['url']
 
-        # جلب محتوى m3u8 من الرابط المستخرج
-        m3u8_response = requests.get(m3u8_url)
-        m3u8_response.raise_for_status()
-        m3u8_content = m3u8_response.text
+    # جلب محتوى m3u8
+    m3u8_response = requests.get(m3u8_url)
+    m3u8_response.raise_for_status()
 
-        # إرجاع المحتوى مباشرةً كاستجابة
-        return Response(m3u8_content, mimetype='application/vnd.apple.mpegurl')
-    except requests.HTTPError as http_err:
-        return f'HTTP error occurred: {http_err}', 500
-    except Exception as err:
-        return f'Other error occurred: {err}', 500
+    # كتابة محتوى m3u8 إلى ملف جديد
+    with open('app.m3u8', 'w') as m3u8_file:
+        m3u8_file.write(m3u8_response.text)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+except requests.HTTPError as e:
+    print(f'HTTP error: {e}', file=sys.stderr)
+except Exception as e:
+    print(f'An error occurred: {e}', file=sys.stderr)
