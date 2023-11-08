1df036da520501ba60840d56b1627ed73dd7f039
kubbar <kubbarr@hotmail.com>
2023-11-08 04:57:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, Response
+import json
 import requests
 
-app = Flask(__name__)
-
-@app.route('/generate_m3u8')
-def generate_m3u8():
+def lambda_handler(event, context):
     # الرابط الذي يحتوي على بيانات التكوين
     config_url = 'https://player.vimeo.com/video/881909784/config?h=0c1725d19a'
 
@@ -22,9 +19,15 @@ def generate_m3u8():
         m3u8_response.raise_for_status()
 
         # إرجاع محتوى m3u8 كاستجابة
-        return Response(m3u8_response.text, mimetype='application/vnd.apple.mpegurl')
+        return {
+            'statusCode': 200,
+            'body': m3u8_response.text,
+            'headers': {
+                'Content-Type': 'application/vnd.apple.mpegurl'
+            }
+        }
     except requests.RequestException as e:
-        return Response(f"An error occurred: {e}", status=500)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
