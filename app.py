114702e48caf6e8a2d68b73dc518f717f6078f08
kubbar <kubbarr@hotmail.com>
2023-11-08 04:42:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+from urllib.parse import urlparse, urlunparse
 import requests
-import re
 
 # استبدل هذا بمسار ملف app_info.txt الخاص بك
 config_url_path = 'app_info.txt'
@@ -11,16 +11,20 @@ response = requests.get(config_url)
 response.raise_for_status()
 config_data = response.json()
 
-# استخرج الـ URL الأساسي من الرابط الذي يحتوي على ملف `.m3u8`
+# استخرج الـ URL الأساسي لـ m3u8
 m3u8_url = config_data['request']['files']['hls']['cdns']['fastly_live']['url']
-base_url = re.match(r'https?://[^/]+/', m3u8_url).group(0)
+parsed_url = urlparse(m3u8_url)
+
+# أزل الجزء الأخير من الـ path (filename) للحصول على الـ base URL
+base_path = '/'.join(parsed_url.path.split('/')[:-1]) + '/'
+base_url = urlunparse(parsed_url._replace(path=base_path))
 
 m3u8_response = requests.get(m3u8_url)
 m3u8_response.raise_for_status()
 m3u8_content = m3u8_response.text
 
-# استبدل جميع المسارات النسبية بمسارات مطلقة باستخدام الـ base_url
-updated_m3u8_content = re.sub(r'chunklist_b(\d+).m3u8', base_url + r'chunklist_b\1.m3u8', m3u8_content)
+# إضافة الـ base URL إلى كل chunklist في المحتوى
+m3u8_content = m3u8_content.replace('chunklist', base_url + 'chunklist')
 
 with open('app.m3u8', 'w') as m3u8_file:
-    m3u8_file.write(updated_m3u8_content)
+    m3u8_file.write(m3u8_content)
