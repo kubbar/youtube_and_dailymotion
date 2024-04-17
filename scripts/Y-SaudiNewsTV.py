87dc91922cba73d2a98ea5f758b491abb3ec1ff2
kubbar <kubbarr@hotmail.com>
2024-04-17 08:28:55 +0300

    Update and rename test.py to Y-SaudiNewsTV.py

--- scripts/Y-SaudiNewsTV.py
+++ scripts/Y-SaudiNewsTV.py
@@ -40,7 +40,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5420722')
 s = requests.Session()
-with open('../test_info.txt') as f:
+with open('../Y-SaudiNewsTV_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
